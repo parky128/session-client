66fdcb6053dddaa08df8bcc7bb32672a6801550c
Kevin Nielsen <knielsen@alertlogic.com>
2019-07-05 08:52:19 -0700

    Added some unit tests for AlSessionDetector

--- test/al-session-detector.spec.ts
+++ test/al-session-detector.spec.ts
@@ -1,4 +1,5 @@
 import { AlSessionDetector, AlConduitClient } from '../src/utilities';
+import { AIMSAuthentication } from '@al/aims';
 import { expect } from 'chai';
 import { describe, before } from 'mocha';
 
@@ -12,7 +13,7 @@ describe('AlSessionDetector', () => {
         } );
     } );
 
-    describe("getAuth0Config", () => {
+    describe(".getAuth0Config", () => {
         it( "should produce configuration values in an expected way", () => {
             let config = sessionDetector.getAuth0Config();
             expect( config.domain ).to.equal("alertlogic.auth0.com");
@@ -25,6 +26,69 @@ describe('AlSessionDetector', () => {
             config = sessionDetector.getAuth0Config( { scope: "openid", prompt: "none" } );
             expect( config.scope ).to.equal( "openid" );
             expect( config.prompt ).to.equal( "none" );
+        } );
+    } );
+
+    describe(".getTokenExpiration", () => {
+        it( "should extract a timestamp from a properly formatted JWT", () => {
+            let timestamp = sessionDetector['getTokenExpiration']("blahblahblah.eyJleHAiOjEwMDAwMDAwLCJzb21ldGhpbmcgZWxzZSI6ImhhaGEifQ==.blahblahblah" );
+            expect( timestamp ).to.equal( 10000000 );
+        } );
+        it( "should return 0 for invalid JWTs", () => {
+            let timestamp;
+
+            //  Wrong wrapper format
+            timestamp = sessionDetector['getTokenExpiration']("totally wrong");
+            expect( timestamp ).to.equal( 0 );
+
+            //  Token information segment is not base64 encoded
+            timestamp = sessionDetector['getTokenExpiration']("blahblahblah.blahblahblah.blahblahblah" );
+            expect( timestamp ).to.equal( 0 );
+
+            //  Token information segment doesn't have an `exp` property
+            timestamp = sessionDetector['getTokenExpiration']("blahblahblah.eyJleHBpcmF0aW9uIjoxMDAwMDAwMCwia2V2aW4iOiJ3YXMgaGVyZSJ9.blahblahblah" );
+            expect( timestamp ).to.equal( 0 );
+        } );
+    } );
+
+    describe(".normalizeAIMSSessionData", () => {
+        it( "should calculate token expiration if it is not already included in the session data object", async () => {
+            let authenticationData = {
+                user: {
+                    id: 1,
+                    name: "Some User"
+                },
+                account: {
+                    id: 1,
+                    name: "Some Account"
+                },
+                token: "blahblahblah.eyJleHAiOjEwMDAwMDAwLCJzb21ldGhpbmcgZWxzZSI6ImhhaGEifQ==.blahblahblah"
+            };
+            let data = await sessionDetector['normalizeAIMSSessionData']( <any>authenticationData );
+            expect( data.token_expiration ).to.equal( 10000000 );
+        } );
+    } );
+
+    describe(".extractUserInfo", () => {
+        it( "should get an accountId/userId pair from validly formatted auth0 identity data", () => {
+            let identityData = {
+                "https://alertlogic.com/": {
+                    sub: "2:10001000-1000"
+                }
+            };
+            let identityInfo = sessionDetector['extractUserInfo']( identityData );
+            expect( identityInfo ).to.be.an( 'object' );
+            expect( identityInfo.accountId ).to.equal( "2" );
+            expect( identityInfo.userId ).to.equal( "10001000-1000" );
+        } );
+
+        it( "should throw in the face of invalid input data", () => {
+            let identityData = {
+                "https://mcdonalds-restaurants.com/": {
+                    sub: "2:10001000-1000"
+                }
+            };
+            expect( () => { sessionDetector['extractUserInfo']( identityData ); } ).to.throw();
         } );
     } );
 
