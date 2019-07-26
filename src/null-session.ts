e495b711866739f061d1d43acc9eb7be7a5bdf9a
Kevin Nielsen <knielsen@alertlogic.com>
2019-07-25 20:14:35 -0700

    Moved `AlDefaultSessionData` to a separate file, as `AlNullSessionDescriptor`.

---
+++ src/null-session.ts
@@ -0,0 +1,62 @@
+import { AIMSSessionDescriptor } from '@al/client';
+
+/**
+ * This session descriptor corresponds to an empty/unauthenticated session state.
+ */
+
+/* tslint:disable:variable-name */
+export const AlNullSessionDescriptor: AIMSSessionDescriptor = {
+    authentication: {
+      user: {
+        id: '0',
+        name: 'Unauthenticated User',
+        email: 'unauthenticated_user@unknown.com',
+        active: false,
+        locked: true,
+        version: 1,
+        created: {
+          at: 0,
+          by: '',
+        },
+        modified: {
+          at: 0,
+          by: '',
+        },
+      },
+      account: {
+        id: '0',
+        name: 'Unknown Company',
+        active: false,
+        accessible_locations: [],
+        default_location: '',
+        created: {
+          at: 0,
+          by: '',
+        },
+        modified: {
+          at: 0,
+          by: '',
+        },
+      },
+      token: '',
+      token_expiration: 0,
+    },
+    acting: {
+      id: '0',
+      name: 'Unknown Company',
+      active: false,
+      version: 1,
+      accessible_locations: [],
+      default_location: '',
+      created: {
+        at: 0,
+        by: '',
+      },
+      modified: {
+        at: 0,
+        by: '',
+      },
+    },
+};
+
+
